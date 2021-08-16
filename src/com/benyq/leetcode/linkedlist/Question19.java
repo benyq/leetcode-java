01b6a66e2c34a50db57246feeee8580ce85ef726
benyq <30992974+benyq@users.noreply.github.com>
2021-08-16 16:26:00 +0800

    2021-08-16, 还有一道链表题

---
+++ src/com/benyq/leetcode/linkedlist/Question19.java
@@ -0,0 +1,46 @@
+package com.benyq.leetcode.linkedlist;
+
+import com.benyq.leetcode.Problem;
+import com.benyq.leetcode.ProblemDifficulty;
+import com.benyq.leetcode.Status;
+
+/**
+ * @author benyq
+ * @emil 1520063035@qq.com
+ * create at 2021/08/16
+ * description:
+ * 题目 19
+ * https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
+ */
+
+@Problem(status = Status.RESOLVED, difficulty = ProblemDifficulty.MEDIUM)
+public class Question19 {
+    public static void main(String[] args) {
+        Question19 question = new Question19();
+        System.out.println(question.removeNthFromEnd(ListNode.createNode(1, 2, 3, 4, 5, 6), 1));
+        System.out.println(question.removeNthFromEnd(ListNode.createNode(1, 2, 3, 4, 5, 6), 2));
+        System.out.println(question.removeNthFromEnd(ListNode.createNode(1, 2, 3, 4, 5, 6), 3));
+        System.out.println(question.removeNthFromEnd(ListNode.createNode(1, 2, 3, 4, 5, 6), 4));
+        System.out.println(question.removeNthFromEnd(ListNode.createNode(1, 2, 3, 4, 5, 6), 5));
+        System.out.println(question.removeNthFromEnd(ListNode.createNode(1, 2, 3, 4, 5, 6), 6));
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast.next != null) {
+            fast = fast.next;
+            if (n-- > 0) {
+                continue;
+            }
+            slow = slow.next;
+        }
+
+        if (n == 1) {
+            return head.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
---
+++ src/com/benyq/leetcode/linkedlist/Question24.java
@@ -0,0 +1,57 @@
+package com.benyq.leetcode.linkedlist;
+
+import com.benyq.leetcode.Problem;
+import com.benyq.leetcode.ProblemDifficulty;
+import com.benyq.leetcode.Status;
+
+/**
+ * @author benyq
+ * @emil 1520063035@qq.com
+ * create at 2021/08/16
+ * description:
+ * 题目 24
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+ */
+
+@Problem(status = Status.REPEAT, difficulty = ProblemDifficulty.MEDIUM)
+public class Question24 {
+    public static void main(String[] args) {
+        Question24 question = new Question24();
+        System.out.println(question.swapPairs(ListNode.createNode(1, 2, 6, 3, 4, 5)));
+        System.out.println(question.swapPairs(ListNode.createNode(2, 3)));
+        System.out.println(question.swapPairs(ListNode.createNode(2, 3, 1)));
+        System.out.println(question.swapPairs(ListNode.createNode(2)));
+        System.out.println(question.swapPairs(ListNode.createNode()));
+
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode dump = new ListNode(0, head);
+
+        ListNode cur = head;
+        ListNode next = head.next;
+        ListNode pre = dump;
+
+        while (next != null) {
+
+            //将 cur 与 next 节点交换，形成 pre -> next -> cur -> next->next 形态
+            ListNode temp = next.next;
+            pre.next = next;
+            next.next = cur;
+            cur.next = temp;
+
+            //移动 pre、cur、next 节点
+            pre = cur;
+            cur = temp;
+            if (temp == null) {
+                break;
+            }
+            next = temp.next;
+        }
+
+        return dump.next;
+    }
+}
